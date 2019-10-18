46e2fb331add916a6650d4149b117a0798709102
CloudZun <info@cloudzun.com>
2019-10-18 15:49:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Abraham was here at 20191009 1537 \n')
+  response.write('Abraham was here at 20191018 1549 \n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
